bb50e650e65339fea0688aa47e64bd1b6bce2410
Daniel Soares <andrade_soares@live.com>
2023-03-28 21:03:39 -0300

    cart controller

---
+++ controllers/cartController.go
@@ -0,0 +1,16 @@
+package controllers
+
+import "github.com/gin-gonic/gin"
+
+func AddToCart() gin.HandlerFunc {
+
+}
+func RemoveItem() gin.HandlerFunc {
+
+}
+func CartCheckout() gin.HandlerFunc {
+
+}
+func InstantBuy() gin.HandlerFunc {
+
+}
---
+++ database/cart.go
@@ -0,0 +1,21 @@
+package database
+
+import "errors"
+
+var (
+	ErrCantFindProduct   = errors.New("")
+	ErrCantDecodeProduct = errors.New("")
+)
+
+func AddToCart() {
+
+}
+func RemoveItem() {
+
+}
+func CartCheckout() {
+
+}
+func InstantBuy() {
+
+}
