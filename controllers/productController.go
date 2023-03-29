ccaad9a3a55dbff2d2bf233950a8f640a41eeda1
Daniel Soares <andrade_soares@live.com>
2023-03-28 21:03:52 -0300

    product controller

---
+++ controllers/productController.go
@@ -0,0 +1,15 @@
+package controllers
+
+import "github.com/gin-gonic/gin"
+
+func ProductView() gin.HandlerFunc {
+
+}
+
+func SearchProduct() gin.HandlerFunc {
+
+}
+
+func AddProduct() gin.HandlerFunc {
+
+}
